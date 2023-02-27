279103416cff702c9e1470fd5715b85fcadacec5
SimoLomba53 <simolomba253@gmail.com>
2023-02-27 16:30:47 +0100

    impostazione di base

---
+++ src/components/dcHeader.vue
@@ -0,0 +1,22 @@
+<script>
+
+</script>
+
+<template>
+    <section>
+        <div class="conteiner">
+            <div>
+                <img src="../assets/img/dc-logo.png" alt="">
+            </div>
+            <div>
+                <ul>
+                    <li>
+
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style></style>
