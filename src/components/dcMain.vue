4cfd544923d60da9c509610d392b1800b2838564
SimoLomba53 <simolomba253@gmail.com>
2023-02-28 18:53:33 +0100

    esercizio completo

--- src/components/dcMain.vue
+++ src/components/dcMain.vue
@@ -20,30 +20,43 @@ export default {
             <DcMainCard v-for="card in cards" :card_data="card" />
 
         </div>
+        <button class="loadmore">LOAD MORE</button>
     </div>
 </template>
 
 <style lang="scss">
 @use "../assets/scss/general.scss" as *;
 
+
+.loadmore {
+    background-color: rgb(57, 57, 255);
+    color: white;
+    margin-bottom: 100px;
+}
+
 .mainsec {
     display: flex;
     flex-wrap: wrap;
     align-items: center;
     justify-content: center;
+    background-color: black;
 
 }
 
+.content li {
+    width: calc(100%/6);
+    list-style: none;
 
+}
 
-.content li img {
-    width: 280px;
-    height: 280px;
-
+.content img {
+    width: 200px;
+    height: 200px;
 }
 
 .content h2 {
     font-size: 10px;
+    color: white;
 }
 
 h1 {
